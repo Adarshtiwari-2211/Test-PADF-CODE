976125fe41b0a05a1ffed5457f82b02056a1d5c4
Adarshtiwari-2211 <adarshtiwari.aohr@gmail.com>
2025-02-28 16:57:12 +0530

    1 add

---
+++ Open Chrome.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+driver = webdriver.Chrome(executable_path="C:\Program Files\chromedriver_win32\chromedriver.exe")
+#driver.get("https://padf-jira.atlassian.net/jira/software/projects/MGS/boards/3")
+# driver.close()
